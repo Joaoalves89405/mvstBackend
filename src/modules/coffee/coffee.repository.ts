5e1e2b1768391af7f82e31c83a73ae0571245b97
Joao Alves <jmca-2000@hotmail.com>
2025-06-19 15:06:00 +0100

    fix: fix

--- src/modules/coffee/coffee.repository.ts
+++ src/modules/coffee/coffee.repository.ts
@@ -1,35 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { Coffee, CoffeeType, Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/providers/db/database.service';
 
 @Injectable()
 export class CoffeeRepository {
   constructor(private readonly db: DatabaseService) {}
 
-  async findAll(): Promise<(Coffee & { type: CoffeeType })[]> {
+  async findAll() {
     return this.db.coffee.findMany({
       include: { type: true },
     });
   }
 
-  async find(params: {
-    where?: { typeId?: string };
-  }): Promise<(Coffee & { type: CoffeeType })[]> {
+  async find(params: { where?: { typeId?: string } }) {
     return this.db.coffee.findMany({
       where: params.where,
       include: { type: true },
     });
   }
 
-  async findByName(name: string): Promise<Coffee | null> {
+  async findByName(name: string) {
     return this.db.coffee.findUnique({ where: { name } });
   }
 
-  async create(data: Prisma.CoffeeCreateInput): Promise<Coffee> {
+  async create(data: any) {
     return this.db.coffee.create({ data });
   }
 
-  async getAllTypes(): Promise<CoffeeType[]> {
+  async getAllTypes() {
     return this.db.coffeeType.findMany();
   }
 }
